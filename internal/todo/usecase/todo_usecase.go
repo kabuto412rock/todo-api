cd5b45991206a076f5fb89184e45708d7c1686c3
ZONG-JIA GU <kabuto412rock@gmail.com>
2025-10-12 08:14:33 +0000

    feat: enhance todo search with title keyword filtering

--- internal/todo/usecase/todo_usecase.go
+++ internal/todo/usecase/todo_usecase.go
@@ -29,8 +29,8 @@ func (uc *TodoUseCase) CreateTodo(title string, dueTime time.Time, done bool) er
 	return uc.repo.Save(todo)
 }
 
-func (uc *TodoUseCase) GetAllTodos(page, limit int) (list []*domain.Todo, total int64, err error) {
-	return uc.repo.FindAll(page, limit)
+func (uc *TodoUseCase) GetAllTodos(page, limit int, title string) (list []*domain.Todo, total int64, err error) {
+	return uc.repo.FindAll(page, limit, title)
 }
 
 func (uc *TodoUseCase) DeleteTodo(id string) error {
